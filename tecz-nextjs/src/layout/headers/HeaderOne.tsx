538bb489f0c6faf0eed777d683655a37a68b7e76
vipin k <vipinkattungal@gmail.com>
2024-12-02 13:12:38 +0530

    logo ny
    logo ny

--- tecz-nextjs/src/layout/headers/HeaderOne.tsx
+++ tecz-nextjs/src/layout/headers/HeaderOne.tsx
@@ -8,7 +8,7 @@ import UseSticky from '@/hooks/UseSticky';
 import { useSelector } from "react-redux";
 import OnPageMenu from './Menu/OnPageMenu';
 import LogoHomeOne from "@/assets/img/logo/logo.png";
-import njlogo from "@/assets/img/logo/njlog1.png";
+import njlogo from "@/assets/img/logo/logosny.png";
 
 import OnePageOffcanvas from './Menu/OnePageOffcanvas';
 import HeaderCart from '@/components/common/HeaderCart';

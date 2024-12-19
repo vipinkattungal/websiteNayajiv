e3b070545755c2058cda5a51cb9567667437cf76
vipin k <vipinkattungal@gmail.com>
2024-12-19 15:46:27 +0530

    updatte title logo
    updatte title logo

--- tecz-nextjs/src/app/layout.tsx
+++ tecz-nextjs/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="description" content="Tecz - Technology & IT Solutions React Next js Template for various types of Sass Products, Software, Startups, App showcases,s and related products/services" />
-        <link rel="icon" href="favicon.png" sizes="any" />
+        <link rel="icon" href="favicon.ico" sizes="512x512" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;500;600;700;800;900&display=swap"

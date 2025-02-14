bcfb4544e159864c13e2e30076f11735cb380e5a
vipin k <vipinkattungal@gmail.com>
2025-02-14 16:27:32 +0530

    update
    update

--- tecz-nextjs/next.config.js
+++ tecz-nextjs/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: "/shop",
+            destination: "/",
+            permanent: true, // Set to false if you want a temporary redirect
+          },
+        ];
+      }
+}
 
 module.exports = nextConfig
